5bdeff6d141cb8b0590c5f5f1c8107481d4ec032
unknown <sjjoseph106@gmail.com>
2022-08-25 16:34:47 +0900

    :technologist: 스타일 환경 구축

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import Router from "@/components/Router";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "@/styles/global-style";
+import { theme } from "@/styles/theme";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
