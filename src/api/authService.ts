66ee6ac6cef5fd3814cafdb74648b3d01045ab64
unknown <sjjoseph106@gmail.com>
2022-09-02 13:03:01 +0900

    :construction: wip

---
+++ src/api/authService.ts
@@ -0,0 +1,8 @@
+const authService = {
+  kakaoLogin() {
+    console.log("카카오");
+    return;
+  },
+};
+
+export default authService;
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,13 @@
 import { Dispatch, SetStateAction } from "react";
+import { authService } from "@/api";
 
 const Auth = ({ setIsLoggedIn }: { setIsLoggedIn: Dispatch<SetStateAction<boolean>> }) => {
-  return <button onClick={() => setIsLoggedIn(true)}>Login</button>;
+  return (
+    <>
+      <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      <button onClick={() => authService.kakaoLogin()}>카카오 로그인</button>
+    </>
+  );
 };
 
 export default Auth;
