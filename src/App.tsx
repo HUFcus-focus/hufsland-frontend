5dbc2f25c990fb30c287ac766f7077db70dd4e97
unknown <sjjoseph106@gmail.com>
2022-09-04 19:34:31 +0900

    :technologist: Recoil DebugObserver 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import Router from "@/components/Router";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "@/styles/global-style";
 import { theme } from "@/styles/theme";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilSnapshot } from "recoil";
+import { useEffect } from "react";
+
+const DebugObserver = () => {
+  const snapshot = useRecoilSnapshot();
+  useEffect(() => {
+    console.log("The following atoms were modified:");
+    for (const node of snapshot.getNodes_UNSTABLE({ isModified: true })) {
+      console.log(node.key, snapshot.getLoadable(node));
+    }
+  }, [snapshot]);
+  return null;
+};
 
 function App() {
   return (
@@ -10,6 +22,7 @@ function App() {
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <RecoilRoot>
+          <DebugObserver />
           <Router />
         </RecoilRoot>
       </ThemeProvider>
