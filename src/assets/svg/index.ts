11e3f1ab19e6af96c2b99c233e7d21a896afbf8f
unknown <sjjoseph106@gmail.com>
2022-10-11 16:40:41 +0900

    :lipstick: Auth 레이아웃

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,2 @@
+export { ReactComponent as AuthBubbleSVG } from "@/assets/svg/auth_bubble.svg";
+export { ReactComponent as LogoWhiteSVG } from "@/assets/svg/logo_white.svg";
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,53 @@
+import styled from "styled-components";
+
+import { kakaoLogin } from "@/assets/img";
+import { AuthBubbleSVG, LogoWhiteSVG } from "@/assets/svg";
 import { URL } from "@/constants";
 
 const Auth = () => {
-  return <a href={URL.KAKAO_AUTH}>카카오 로그인</a>;
+  return (
+    <AuthLayout>
+      <LogoWhiteSVG width='80px' style={{ marginTop: "80px" }} />
+      <AuthContainer>
+        <AuthText>
+          <span style={{ fontWeight: "bold" }}>소셜로그인</span>으로
+          <br /> 간편하게 시작해보세요.
+        </AuthText>
+        <AuthBubbleSVG />
+        <a href={URL.KAKAO_AUTH}>
+          <img src={kakaoLogin} style={{ marginTop: "20px", width: "280px" }} alt='kaka_login' />
+        </a>
+      </AuthContainer>
+    </AuthLayout>
+  );
 };
 
 export default Auth;
+
+const AuthLayout = styled.div`
+  background-color: ${({ theme }) => theme.color.blue};
+  position: relative;
+  text-align: center;
+  height: 100%;
+`;
+
+const AuthContainer = styled.div`
+  background-color: ${({ theme }) => theme.color.white};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 75%;
+  border-radius: 50px 50px 0px 0px;
+`;
+
+const AuthText = styled.div`
+  color: ${({ theme }) => theme.color.blue};
+  font-family: "Noto Sans KR";
+  margin-top: 50px;
+  margin-bottom: 70px;
+  font-size: 24px;
+  line-height: 35px;
+`;
